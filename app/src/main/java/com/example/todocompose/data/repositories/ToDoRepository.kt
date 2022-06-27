b957e16569ab97a38d1b876b4d5179ae3a03ad3b
f-arslan <buildsmstff@gmail.com>
2022-06-27 23:32:44 +0300

    SharedViewModel and ToDoRepository

---
+++ app/src/main/java/com/example/todocompose/data/repositories/ToDoRepository.kt
@@ -0,0 +1,39 @@
+package com.example.todocompose.data.repositories
+
+import com.example.todocompose.data.ToDoDao
+import com.example.todocompose.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ViewModelScoped
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortedByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortedByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDao.getSelectedTask(taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDao.addTask(toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDao.updateTask(toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDao.deleteTask(toDoTask)
+    }
+
+    suspend fun deleteAllTasks() {
+        toDoDao.deleteAllTasks()
+    }
+
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>> {
+        return toDoDao.searchDatabase(searchQuery)
+    }
+}
